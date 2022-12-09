29882a6f50282c0a6880afebddd3ba94df5c2e98
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2022-12-09 12:08:27 +0700

    fixed

--- resources/views/product/detail.blade.php
+++ resources/views/product/detail.blade.php
@@ -12,9 +12,104 @@ $img = json_decode($product->images);
 {{url(''.$img[0])}}
 @endsection
 @section('css')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
+<style>
+   .swiper {
+   width: 100%;
+   height: 100%;
+   }
+
+   .swiper-slide {
+   text-align: center;
+   font-size: 18px;
+   background: #fff;
+
+   /* Center slide text vertically */
+   display: -webkit-box;
+   display: -ms-flexbox;
+   display: -webkit-flex;
+   display: flex;
+   -webkit-box-pack: center;
+   -ms-flex-pack: center;
+   -webkit-justify-content: center;
+   justify-content: center;
+   -webkit-box-align: center;
+   -ms-flex-align: center;
+   -webkit-align-items: center;
+   align-items: center;
+   }
+
+   .swiper-slide img {
+   display: block;
+   width: 100%;
+   height: 100%;
+   object-fit: cover;
+   }
+
+   .swiper {
+   width: 100%;
+   height: 300px;
+   margin-left: auto;
+   margin-right: auto;
+   }
+
+   .swiper-slide {
+   background-size: cover;
+   background-position: center;
+   }
+
+   .mySwiper2 {
+   height: 80%;
+   width: 100%;
+   }
+
+   .mySwiper {
+   height: 20%;
+   box-sizing: border-box;
+   padding: 10px 0;
+   }
+
+   .mySwiper .swiper-slide {
+   width: 25%;
+   height: 100%;
+   opacity: 0.4;
+   }
+
+   .mySwiper .swiper-slide-thumb-active {
+   opacity: 1;
+   }
+
+   .swiper-slide img {
+   display: block;
+   width: 100%;
+   height: 100%;
+   object-fit: cover;
+   }
+</style>
 @endsection
 @section('js')
+<!-- Swiper JS -->
+<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
 
+<!-- Initialize Swiper -->
+<script>
+   var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+   });
+   var swiper2 = new Swiper(".mySwiper2", {
+      spaceBetween: 10,
+      navigation: {
+         nextEl: ".swiper-button-next",
+         prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+         swiper: swiper,
+      },
+   });
+</script>
 @endsection
 @section('content')
 @php
@@ -23,15 +118,23 @@ $img = json_decode($product->images);
 <div class="wrap-main w-clear">
    <div class="grid-pro-detail w-clear">
       <div class="left-pro-detail w-clear">
-         <a id="Zoom-1" class="MagicZoom" data-options="zoomMode: off; hint: off; rightClick: true; selectorTrigger: hover; expandCaption: false; history: false;" href="{{$img[0]}}" title="{{languageName($product->name)}}">
-            <img src="{{$img[0]}}" alt="{{languageName($product->name)}}">
-         </a>
-         <div class="gallery-thumb-pro">
-            <div class=" slick-img-thumb">
+         <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2" >
+            <div class="swiper-wrapper">
                @foreach ($img as $key => $item)
-               <a class="thumb-pro-detail" data-zoom-id="Zoom-{{$key+1}}" href="{{$item}}" title="{{languageName($product->name)}}">
-                  <img src="{{$item}}" alt="{{languageName($product->name)}}">
-               </a>
+               <div class="swiper-slide">
+                  <img src="{{$item}}" />
+               </div>
+               @endforeach
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+         </div>
+         <div thumbsSlider="" class="swiper mySwiper">
+            <div class="swiper-wrapper">
+               @foreach ($img as $key => $item)
+               <div class="swiper-slide">
+                  <img src="{{$item}}" />
+               </div>
                @endforeach
             </div>
          </div>
